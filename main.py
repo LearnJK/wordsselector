30155338fe570f4a4126724c56cf4c65cf067ace
Juan Linares <juanklg25@gmail.com>
2021-08-23 21:25:59 -0500

    update word selector

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+## todaviano hemos iterado el secret
 app.config['SECRET_KEY'] = 'secret!'
+## debemos limitar la conexion del cors con solo el link de origen de la app de produccion
 socketio = SocketIO(app,cors_allowed_origins="*")
 
 @app.route('/')
+## Renderiza el landing page
 def index(): return render_template('index.html')
 
 @socketio.on('FtoBMsj')
@@ -16,12 +19,7 @@ def handleFtoBCmd(json):
 def handleFtoBCmd(json):
     print(str(json))
     send(json, broadcast = True)
-#leer en flask soketio como enviar por broadcast a todas las paginas con el mensaje correspondienmte
+## leer en flask soketio como enviar por broadcast a todas las paginas con el mensaje correspondienmte
 
 if __name__ == '__main__':
     socketio.run(app)
-
-# @socketio.on('m')
-# def handleM(m):
-#     print('M: ',m)
-#     send(m, broadcast = True)
